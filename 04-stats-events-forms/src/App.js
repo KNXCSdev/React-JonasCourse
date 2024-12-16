dc24c28dd705a8722f2482b94201142ce425dc7b
KNXCSdev <michal.miksho@gmail.com>
2024-12-16 18:22:12 +0100

    Appv1,Appv2,Reusable Components

--- 04-stats-events-forms/src/App.js
+++ 04-stats-events-forms/src/App.js
@@ -6,7 +6,6 @@ export default function App() {
   return (
     <div>
       <Steps></Steps>
-      <Steps></Steps>
     </div>
   );
 }
@@ -41,20 +40,45 @@ function Steps() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+          <StepMessage step={step}>
+            {messages[step - 1]}
+            <div className="buttons">
+              <Button
+                textColor="#333"
+                bgColor="#e7e7e7"
+                onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
+              >
+                Learn How
+              </Button>
+            </div>
+          </StepMessage>
 
           <div className="buttons">
-            <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handlePrevious}>
-              Previous
-            </button>
-            <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handleNext}>
-              Next
-            </button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious} text="Previous">
+              <span>👈 Previous</span>
+            </Button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext} text="Next">
+              <span>Next 👉</span>
+            </Button>
           </div>
         </div>
       )}
     </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3> Step {step} </h3> {children}
+    </div>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  return (
+    <button style={{ backgroundColor: bgColor, color: textColor }} onClick={onClick}>
+      {children}
+    </button>
   );
 }

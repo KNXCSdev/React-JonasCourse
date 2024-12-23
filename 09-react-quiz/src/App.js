36c7c7a647d9af7b9343077e44864d9b63fbd484
KNXCSdev <michal.miksho@gmail.com>
2024-12-23 19:46:35 +0100

    ready,loading,error Components

--- 09-react-quiz/src/App.js
+++ 09-react-quiz/src/App.js
@@ -1,9 +1,57 @@
-import DateCounter from "./DateCounter.js";
+// import DateCounter from "./DateCounter.js";
+
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import Ready from "./Ready";
+
+const initialState = {
+  questions: [],
+
+  //Loading,Error,Ready,Active,Finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    case "ready":
+      return { ...state, questions: action.payload, status: "active" };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { status, questions } = state;
+
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter></DateCounter>
+    <div className="app">
+      <Header></Header>
+
+      <Main>
+        {status === "loading" && <Loader></Loader>}
+        {status === "error" && <Error></Error>}
+        {status === "ready" && <Ready numQuestions={numQuestions}></Ready>}
+      </Main>
     </div>
   );
 }
